7b50bde02765192f45642afbf922c4666992eb32
Jackie <kamprat2@gmail.com>
2020-03-29 12:20:16 -0400

    button handler for dashboard

--- plots.js
+++ plots.js
@@ -1,20 +1,35 @@
 // 1. Use the D3 library to read in `samples.json`.
 
-function buildMetadata(sample) {
-    d3.json("samples.json").then((data) => console.log(data))};
+// var data = d3.json("samples.json").then(function(data) {
+//     console.log(data)
+//  });
 
-buildMetadata();
+
+// console.log(data.metadata);
 
 
 // 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 
 // * Use `sample_values` as the values for the bar chart.
-
 // * Use `otu_ids` as the labels for the bar chart.
-
 // * Use `otu_labels` as the hovertext for the chart.
 
-//   ![bar Chart](Images/hw01.png)
+//Submit Button Handler for the dropdown
+function handleSubmit() {
+  //prevent the page from reloading
+  d3.event.preventDefault();
+
+  //select the input value
+  var testSubject = d3.select("#selDataset").node().value;
+  console.log(testSubject);
+
+  //clear the input value
+  d3.select("#selDataset").node().value = "";
+
+  //Build the plot with the testsubject data
+  buildBarChart(testSubject);
+};
+
 
 // 3. Create a bubble chart that displays each sample.
 
@@ -29,6 +44,9 @@ buildMetadata();
 // * Use `otu_labels` for the text values.
 
 // ![Bubble Chart](Images/bubble_chart.png)
+
+
+
 
 // 4. Display the sample metadata, i.e., an individual's demographic information.
 
