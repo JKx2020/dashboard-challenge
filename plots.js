3866aefe8998d63e0f445bc10d1e04cf26804c67
Jackie <kamprat2@gmail.com>
2020-03-29 17:01:09 -0400

    updates

--- plots.js
+++ plots.js
@@ -9,13 +9,18 @@
 //create button handler for the dashboard to refresh all graphs on page
 
 //populate the drop down with the sample IDs
-var sel = document.getElementById('selDataSet');
+// var names = d3.json("names.json", function(data) {console.log(data);
+// });
+// console.log(names[0]);
 
-for (var i = 0, i < names.length; i++) {
-  var option = document.createElement('option');
-  option.value = names[i];
-  sel.appendChild(option);
-};
+
+// var sel = document.getElementById('selDataSet');
+
+// for (var i = 0; i < names.length; i++) {
+//   var option = document.createElement('option');
+//   option.value = names[i];
+//   sel.appendChild(option);
+// };
 
 //Submit Button Handler for the dropdown
 function handleSubmit() {
@@ -41,47 +46,48 @@ function handleSubmit() {
 // * Use `otu_labels` as the hovertext for the chart.
 
 //create the barchart & bubble functions
-function buildCharts(data) {
+function buildCharts() {
+  d3.json("samples.json").then(function(data) {
 
-  //grab the values for the charts and lables
-  var sampleValues = data.samples.sample_values;
-  var otuIds = data.samples.otu_ids;
-  var otuLables = data.samples.otu_lables;
+    //grab the values for the charts and lables
+    var sampleValues = data.samples.sample_values;
+    var otuIds = data.samples.otu_ids;
+    var otuLables = data.samples.otu_lables;
 
-  // 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-  var barTrace = {
-    type: "bar",
-    orientation: "h",
-    x: sampleValues,
-    y: otuIds,
-    hovertext: otuLables,
+    // 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+    var barTrace = {
+      type: "bar",
+      orientation: "h",
+      x: sampleValues,
+      y: otuIds,
+      hovertext: otuLables,
+      };
+    
+    var barLayout = {
+      title: "Top 10 OTU's found in the Test Subject"
+      };
+
+    Plotly.newPlot("bar", barTrace, barLayout);
+
+    // 3. Create a bubble chart that displays each sample.
+    var bubbleTrace = {
+      type: "bubble",
+      x: otuIds,
+      y: sampleValues,
+      mode: 'markers',
+      marker: {
+        size: sampleValues,
+        color: otuIds},
+      text: otuLables
     };
-  
-  var barLayout = {
-    title: "Top 10 OTU's found in the Test Subject"
+
+    var bubbleLayout = {
+      title: "OTUs Most Commonly Found in Belly Buttons"
     };
 
-  Plotly.newPlot("bar", barTrace, barLayout);
-
-  // 3. Create a bubble chart that displays each sample.
-  var bubbleTrace = {
-    type: "bubble",
-    x: otuIds,
-    y: sampleValues,
-    mode: 'markers',
-    marker: {
-      size: sampleValues,
-      color: otuIds},
-    text: otuLables
-  };
-
-  var bubbleLayout = {
-    title: "OTUs Most Commonly Found in Belly Buttons"
-  };
-
-  Plotly.newPlot("bubble", bubbleLayout, bubbleLayout);
+    Plotly.newPlot("bubble", bubbleLayout, bubbleLayout);
+  });
 };
-
 
 // 4. Display the sample metadata, i.e., an individual's demographic information.
 
@@ -102,11 +108,13 @@ function buildDemoInfo(metadata) {
    testData.forEach(info => {
      var row = panelRow.append("panel-body");
 
-     Object.entries(info).forEach(([key, value])=> row.text(key, ":", value);
+     Object.entries(info).forEach(([key, value])=> row.text(key, ":", value));
 
      });
    };
 
+// Add event listener for submit button
+d3.select("#selDataset").on("click", handleSubmit);
 
 // 5. Display each key-value pair from the metadata JSON object somewhere on the page.
 
